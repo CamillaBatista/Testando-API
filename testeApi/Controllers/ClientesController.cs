45440bf1f1cd2dff25e9d9620818f1b80a4908d3
Camilla Batista <camillabatista95@gmail.com>
2021-03-08 02:55:16 -0300

    Primeiro commit

---
+++ testeApi/Controllers/ClientesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testeApi.Models;
+
+namespace testeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        [HttpGet]
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        [HttpPost]
+        public void Post(string nome)
+        {
+            clientes.Add(new Cliente(nome));
+        }
+    }
+}
